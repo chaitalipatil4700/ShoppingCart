a9bfd761d52f4aa69e4c2d72aa30047e454e8d71
chaitalipatil4700 <68694992+chaitalipatil4700@users.noreply.github.com>
2022-04-12 20:38:13 +0530

    Delete ShoppingCart directory

--- ShoppingCart/src/main/java/exception/ItemNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package exception;
-
-public class ItemNotFoundException extends Exception {
-    public ItemNotFoundException() {
-        System.out.println("Item Not Found");
-    }
-}
--- ShoppingCart/src/main/java/model/Item.java
+++
@@ -1,5 +0,0 @@
-package model;
-
-public enum Item {
-    APPLE, CARROT, BANANA
-}
